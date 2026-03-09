b995e71081c3aa23cb7d426cec02d896aeb3b686
lazKorsan <ahmeteldes2002@gmail.com>
2026-03-10 02:14:43 +0300

    nested frames testi eklendi

--- inLineTest/US020NestesFrameTest.py
+++ inLineTest/US020NestesFrameTest.py
@@ -1,14 +1,50 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from utils.description_utils import description_utils
 
-driver = webdriver.Chrome()
-driver.get("https://the-internet.herokuapp.com/nested_frames")
+# WebDriver'ı başlat
+driver = webdriver.Chrome()  # veya kullandığınız tarayıcı için uygun WebDriver'ı belirtin
 
-    # (By, value) tuple ile:
-result = description_utils(
-        driver,
-        locator=(By.XPATH, '/html/frameset/frame[1]'),
-        highlight_colour="#00BFFF",
-        circle_colour="#FF6347",
-)
+try:
+    # Sayfayı aç
+    driver.get("https://the-internet.herokuapp.com/nested_frames")
+
+    # Üst çerçeveye geçiş (frame-top)
+    driver.switch_to.frame("frame-top")
+
+    # Sol çerçeveye geçiş (frame-left)
+    driver.switch_to.frame("frame-left")
+    # LEFT metnini bul ve yazdır
+    left_element = driver.find_element(By.XPATH, "//body")
+    print("LEFT:", left_element.text)
+
+    # Kapsayıcıdan çık
+    driver.switch_to.parent_frame()
+
+    # Orta çerçeveye geçiş (frame-middle)
+    driver.switch_to.frame("frame-middle")
+    # MIDDLE metnini bul ve yazdır
+    middle_element = driver.find_element(By.XPATH, "//div[@id='content']")
+    print("MIDDLE:", middle_element.text)
+
+    # Kapsayıcıdan çık
+    driver.switch_to.parent_frame()
+
+    # Sağ çerçeveye geçiş (frame-right)
+    driver.switch_to.frame("frame-right")
+    # RIGHT metnini bul ve yazdır
+    right_element = driver.find_element(By.XPATH, "//body")
+    print("RIGHT:", right_element.text)
+
+    # Asıl çerçeveye geri dön
+    driver.switch_to.default_content()
+
+    # Alt çerçeveye geçiş (frame-bottom)
+    driver.switch_to.frame("frame-bottom")
+
+    # BOTTOM metnini bul ve yazdır
+    bottom_element = driver.find_element(By.XPATH, "//body")
+    print("BOTTOM:", bottom_element.text)
+
+finally:
+    # Tarayıcıyı kapat
+    driver.quit()
